dd04f17d1f64fb74d3c9447f8037977f77436278
hedenica <hedenica@hotmail.com>
2020-07-06 09:27:16 -0300

    ✨  added  call to api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+import Header from './components/Header';
+import CharacterGrid from './components/Characters/CharacterGrid';
+import Search from './components/Ui/Search';
+
 import './App.css';
 
 const App = () =>  {
+  const [characters, setCharacters] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [query, setQuery] = useState('');
+
+  useEffect(() => {
+    const fetchCharacters = async () => {
+      const result = await axios.get(`https://breakingbadapi.com/api/characters?name=${query}`);
+
+      setCharacters(result.data);
+      setIsLoading(false);
+    }
+
+    fetchCharacters();
+  }, [query])
+
   return (
     <div className="container">
-      heeey
+      <Header />
+      <Search getQuery={(q) => setQuery(q)} />
+      <CharacterGrid isLoading={isLoading} characters={characters} />
     </div>
   );
 }
